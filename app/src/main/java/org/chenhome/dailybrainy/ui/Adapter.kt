2acb6ac52bbc49db2a9d50c023225ba278a67bd5
johnny <johnny@chenhome.org>
2020-09-27 12:15:51 -0700

    Material UI - VoteSketchFrag - countdown works - remove obsolete layouts, adapters

--- app/src/main/java/org/chenhome/dailybrainy/ui/Adapter.kt
+++ app/src/main/java/org/chenhome/dailybrainy/ui/Adapter.kt
@@ -6,41 +6,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.chenhome.dailybrainy.databinding.ItemIdeaBinding
 import org.chenhome.dailybrainy.databinding.ItemSketchBinding
-import org.chenhome.dailybrainy.databinding.ViewGameItemPlayerBinding
 import org.chenhome.dailybrainy.repo.Idea
-import org.chenhome.dailybrainy.repo.PlayerSession
 import org.chenhome.dailybrainy.repo.game.Sketch
 import org.jetbrains.annotations.NotNull
-
-internal class PlayerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    var players: MutableList<PlayerSession> = mutableListOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return PlayerVH(ViewGameItemPlayerBinding.inflate(LayoutInflater.from(parent.context),
-            parent,
-            false))
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as PlayerVH).bind(players[position])
-    }
-
-    override fun getItemCount(): Int = players.size
-
-    class PlayerVH(val binding: @NotNull ViewGameItemPlayerBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(player: PlayerSession) {
-            binding.player = player
-            binding.playerAvatar.setImageResource(player.avatarImage().imgResId)
-        }
-    }
-
-}
 
 internal class IdeaAdapter(val showVotes: Boolean) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
